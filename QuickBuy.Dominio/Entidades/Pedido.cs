5717aa0733431584e813a064030c2711d272348c
Cleufis Rangel <cleufis@gmail.com>
2020-02-12 14:20:30 -0300

    Criação das entidades.

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,38 @@
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+
+        public DateTime DataPedido { get; set; }
+
+        public int IdUsuario { get; set; }
+
+        public Usuario Usuario { get; set; }
+
+        public DateTime DataPrevisaoEntrega { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string EnderecoCompletp { get; set; }
+
+        public int NumeroEndereco { get; set; }
+
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+
+        public FormaPagamento FormaPagamento { get; set; }
+
+
+    }
+}
